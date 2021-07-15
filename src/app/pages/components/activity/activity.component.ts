93e5a78e7dcd8b94a794ec9698fd92169c4da1a1
FahrasNH <fahras1998@yahoo.com>
2021-07-15 22:38:55 +0700

    Mapping activity

--- src/app/pages/components/activity/activity.component.ts
+++ src/app/pages/components/activity/activity.component.ts
@@ -6,5 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity.component.scss']
 })
 export class ActivityComponent implements OnInit {
+  activities: any[] = [
+    {
+      name: 'Michael',
+      activity: 'connected',
+      title: 'How to become an expert',
+      updated: '2 seconds ago'
+    },
+    {
+      name: 'John Smith',
+      activity: 'add a new video',
+      title: 'Creative people mus follow',
+      updated: '1 day ago'
+    },
+    {
+      name: 'Batista',
+      activity: 'shared a document',
+      title: 'How to become an expert',
+      updated: '1 hour ago'
+    },
+    {
+      name: 'Rock Lee',
+      activity: 'upload a new video',
+      title: 'How to become an expert',
+      updated: '19 seconds ago'
+    },
+    {
+      name: 'Chelsea',
+      activity: 'Like a video',
+      title: 'Some marketing tricks',
+      updated: '44 minutes ago'
+    }
+  ];
+
   ngOnInit(): void {}
 }
