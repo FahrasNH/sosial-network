590fd04399148fd905ad226de273d1dbd28fd08a
FahrasNH <fahras1998@yahoo.com>
2021-07-15 14:22:28 +0700

    Menubar style

--- src/app/pages/components/navbar/navbar.component.ts
+++ src/app/pages/components/navbar/navbar.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  arrMenu: string[] = [];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.arrMenu = [
+      'Videos',
+      'People',
+      'Documents',
+      'Events',
+      'Communities',
+      'Favourites',
+      'Channel'
+    ];
+  }
 }
