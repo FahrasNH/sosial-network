232153d66211d89c24098e476c56a2fdbb7668f8
FahrasNH <fahras1998@yahoo.com>
2021-07-15 19:52:07 +0700

    Create footer

--- src/app/pages/components/home.module.ts
+++ src/app/pages/components/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent, ContentComponent],
+  declarations: [NavbarComponent, ContentComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [NavbarComponent, ContentComponent]
+  exports: [NavbarComponent, ContentComponent, FooterComponent]
 })
 export class HomeModule {}
