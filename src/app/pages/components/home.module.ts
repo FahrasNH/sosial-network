0f92fefe64945187e47e19872fb9cd95d26091ca
FahrasNH <fahras1998@yahoo.com>
2021-07-15 19:05:20 +0700

    Show content social

--- src/app/pages/components/home.module.ts
+++ src/app/pages/components/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, ContentComponent],
   imports: [CommonModule],
-  exports: [NavbarComponent]
+  exports: [NavbarComponent, ContentComponent]
 })
 export class HomeModule {}
